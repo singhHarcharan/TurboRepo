d4806acab0db187d88442baf7422997ddcac9b99
singhHarcharan <harcharansingh198400@gmail.com>
2026-01-14 17:22:32 +0530

    Added common module in Monorepo packages

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
+import { BACKEND_URL, VALUE} from "@repo/common"
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
+    console.log(BACKEND_URL);
+    console.log(VALUE);
     return res.json({
         "message": "Hello world"
     });
