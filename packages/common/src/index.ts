5efa2cddf4bfb433e3b46d4705a9b1738c6ddd9e
singhHarcharan <harcharansingh198400@gmail.com>
2026-01-14 17:22:32 +0530

    Added common module in Monorepo packages

---
+++ packages/common/src/index.ts
@@ -0,0 +1,5 @@
+// npm init -y      --> Initialize package.json 
+// npx tsc --init   --> Initialize tsconfig.json file 
+
+export const VALUE = "Harcharanpreet Singh"
+export const BACKEND_URL = "http://api.google.com";
