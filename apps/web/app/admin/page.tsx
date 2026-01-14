46fbf4d66683e03e1e1a8005f18444eea75f9976
singhHarcharan <harcharansingh198400@gmail.com>
2026-01-14 13:23:57 +0530

    Added react app in monorepo

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import { Admin } from "@repo/ui/admin";
+import React from "react";
+
+export default function () {
+    return (
+        <div>
+            Hi from Admin page
+            <Admin />
+        </div>
+    )
+}
